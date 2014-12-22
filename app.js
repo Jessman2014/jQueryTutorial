c91ba8d586ccfa7379feda0299f80aad73fec4d8
Jesse Dahir-Kanehl <daisysgreenhouse@gmail.com>
2014-12-22 07:06:39 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+/*
+
+For this quiz, can you use this script, which is in the <head> of index.html,
+to change the boring placeholder image to a picture of a cute puppy?
+
+Remember, you'll need to pass a function into the jQuery object to run 
+when the document is ready.
+
+Here's a URL for a picture of a puppy: http://placepuppy.it/350/150
+
+Good luck!
+
+*/
+
+function puppy() {
+var image = $(".article-item").find("img");
+image.attr("src", "http://placepuppy.it/350/150");
+}
+
+$(puppy)
